dde53f8bf991eeee97b0675b81e6ee3fed334323
Tenzing Lama <tenzingnyimalama@gmail.com>
2024-03-17 15:29:29 -0400

    updated setup-jenkins script

--- setup-jenkins.sh
+++ setup-jenkins.sh
@@ -1,11 +1,12 @@
-sudo yum upgrade -y
-sudo wget -O /etc/yum.repos.d/jenkins.repo \
+#!/bin/bash
+yum upgrade -y
+wget -O /etc/yum.repos.d/jenkins.repo \
     https://pkg.jenkins.io/redhat-stable/jenkins.repo #Add Jenkins Repository
-sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key
-sudo dnf install java-17-amazon-corretto -y #Install Java
-sudo yum install jenkins -y
-sudo systemctl enable jenkins
-sudo systemctl start jenkins
-sudo systemctl status jenkins
+rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key
+dnf install java-17-amazon-corretto -y #Install Java
+yum install jenkins -y
+systemctl enable jenkins
+systemctl start jenkins
+systemctl status jenkins
 
 #Connect to http://<your_server_public_DNS>:8080 from your browser.
